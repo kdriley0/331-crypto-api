010f32bfabbde1cd273e7efd0423a6f17bc3b3bb
Kdriley0 <kdriley0@frostburg.edu>
2018-11-02 10:05:04 -0400

    another fail

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const crypto = require("./crypto");
 const yargs = require("yargs");
 //my function coin so I can have different coins
 var coin = {  symbol: "",price: 0};
-var coins= [];//hopefully so i can have multiple coins 
+var coins= [];
 const argv = yargs
   .options({
     c: {
@@ -20,14 +20,20 @@ const argv = yargs
   crypto.getCrypto(argv.c, (err, results) => {
 	  if(err){
 		  console.log(err)
-	  } else { console.log(typeof(results)), console.log(`your_crypto: ${argv.c},  \nyour_current_state_is:  
+	  } else { console.log(typeof(results.USD)), console.log(`your_crypto: ${argv.c},  \nyour_current_state_is:  
 		${JSON.stringify(results)},`//key "USD" dictonary
 		);
 		
 		
-		var pointNum = parseFloat(results);
-		console.log(pointNum);//NaN
-		//let coinV = results.price;//undefined
+		const cstr = JSON.stringify(results);
+		let fcn = (cstr) =>{
+		  coins=cstr.split(" ");
+		  
+		}
+		console.log(coins);
+		
+		
+		//let coinV = results.price;
 //	console.log(`your coin is ${argv.c} and its price is ${coinV}`);
 		
 	
