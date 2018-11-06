b69e9c551b43d22cdb50b46b9c027166bf43c1f7
Kdriley0 <kdriley0@frostburg.edu>
2018-11-06 13:28:57 -0500

    all commented out

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+
 const crypto = require("./crypto");
 const yargs = require("yargs");
 //my function coin so I can have different coins  I am going to try to store it in another file like we did with notes
 //var coin = {  symbol: "",price: 0};
-const coins = require("./coins")
+
+
+
+//const coins = require("./coins") // a failed attempt at doing what you showed us in the note file
+
+//declaring variables to be used later
 var coi;
 const argv = yargs
   .options({
@@ -17,26 +23,25 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
   
-  var i=0;
   //console.log("yargs", yargs);
- crypto.getCrypto(argv.c, argv.n, (err, results) => {
+ crypto.get_crypto(argv.c, argv.n, (err, results) => {
 	  if(err){
 		  console.log(err)
 	  } else { 
 		
+			
+			const cstr = JSON.stringify(results);//assigning the string version of results to the const cstr or constant string
+			const splitStr=cstr.split(":");		 //splitting the string by : 
+			var token=argv.c;
+			
+			
+			coi = splitStr[3].substring(0,(splitStr[2].length-2));   //grabbing the USD value of the str
+			coi=parseFloat(coi);								     // and making it into a float
+		//console.log(token); 
+			var number=parseFloat(argv.n);						     // making a float out of the numbed given 
+			number*=coi;										     //seting the number equal to the total worth of the coins
 		
-		const cstr = JSON.stringify(results);
-		const splitStr=cstr.split(":");
-		
-		var token=argv.c;
-	//	console.log(splitStr[2]);
-		coi = splitStr[3].substring(0,(splitStr[2].length-2));
-		coi=parseFloat(coi);
-	console.log(token);
-		var number=parseFloat(argv.n);
-		number*=coi;
-	
-			console.log(`your ${token} coins are worth ${number}$` );
+			console.log(`your ${token} coins are worth ${number}$` );//showing the result
 	
 		  }
 	  
